c107171d994516264dc9bd3319c3b14c2fe7fcdc
Lynn Wanjiru <lynnwanjiruu@gmail.com>
2026-03-24 21:39:30 +0300

    2nd commit

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+  },
 })
